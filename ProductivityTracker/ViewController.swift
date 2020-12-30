b54862afbc2a8ca53515c977b05024e7bc6e952b
Ilja Patrushev <slowtnr@gmail.com>
2020-12-30 10:25:53 +0200

    Initial Commit

---
+++ ProductivityTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductivityTracker
+//
+//  Created by Ilja Patrushev on 30.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
